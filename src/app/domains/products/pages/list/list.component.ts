a4147f1738a5ec7081ca430ade52d8a7da87aa10
Camilo Del Valle Ledesma <delvallec@globalhitss.com>
2024-04-08 17:17:49 -0500

    Clase 42 ok

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { ProductComponent } from './../../components/product/product.component'
 import { Product } from '../../../shared/models/product.model';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [ProductComponent, CommonModule],
+  imports: [ProductComponent, CommonModule, HeaderComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
@@ -44,6 +45,34 @@ export class ListComponent {
         image: 'https://picsum.photos/320/320?r=5',
         creationAt: new Date().toISOString()
       },
+      {
+        id: Date.now(),
+        title: 'Camisa',
+        price: 5,
+        image: 'https://picsum.photos/320/320?r=2',
+        creationAt: new Date().toISOString()
+        },
+        {
+        id: Date.now(),
+        title: 'Pantalón',
+        price: 6,
+        image: 'https://picsum.photos/320/320?r=3',
+        creationAt: new Date().toISOString()
+        },
+        {
+          id: Date.now(),
+          title: 'Medias',
+          price: 2,
+          image: 'https://picsum.photos/320/320?r=4',
+          creationAt: new Date().toISOString()
+        },
+        {
+          id: Date.now(),
+          title: 'Zapatos',
+          price: 10,
+          image: 'https://picsum.photos/320/320?r=5',
+          creationAt: new Date().toISOString()
+        }
     ];
     this.products.set(initProducts);
   }
