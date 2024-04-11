e0cad3770514e4c9b3b34e0e066d98c86a2e6e5e
Camilo Del Valle Ledesma <delvallec@globalhitss.com>
2024-04-11 17:32:06 -0500

    Hasta clase 54, revisar última parte

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -2,11 +2,12 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { RequiredValidator } from '@angular/forms';
 import { Product } from '../../../shared/models/product.model';
 import { CommonModule } from '@angular/common';
+import { TimeAgoPipe } from '@shared/pipes/time-ago.pipe';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TimeAgoPipe],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
